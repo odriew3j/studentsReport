898772182d6b652271efe4d77e0116f5533524fd
Msv <mohammad.mousavi3j@gmail.com>
2024-06-11 18:00:45 +0330

    debug agian and start static front

--- reportCard/urls.py
+++ reportCard/urls.py
@@ -17,9 +17,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from studentReport import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     
     path('studentReport/', include('studentReport.urls')),
     path('admin/', admin.site.urls),
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- studentReport/views.py
+++ studentReport/views.py
@@ -6,6 +6,7 @@ import os
 
 def students_list(request):
     StudentReport = studentReport.objects.all()
-    template_path = 'studentReport/list.html'
-    context = {'studentReport': StudentReport}
-    return render(request, template_path, context)
+    # template_path = 'studentReport/list.html'
+    # context = {'studentReport': StudentReport}
+    # return render(request, template_path, context)
+    return render(request, "studentReport/list.html", {"StudentReport":StudentReport})
